a293921f952492948d099b9a1b316dba12bfeb6d
kriti009 <kriti.ritu.dew@gmail.com>
2019-06-07 14:49:54 +0530

    gender change

--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var productSchema = new mongoose.Schema({
     
-    gender : {type: String , enum : ['m' ,'f']},
+    gender : {type: String , enum : ['male' ,'female']},
     image_url : {type: String},
     service_type: {type : String},
     category: {type: String , enum: ['formal', 'informal']},
